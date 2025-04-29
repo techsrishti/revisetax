57bb9a9ecfc95b456fb3efd894e34fa915938350
Hemanth <68335586+itsmrhem@users.noreply.github.com>
2025-04-29 15:44:55 +0530

    init

---
+++ next.config.mjs
@@ -0,0 +1,15 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    unoptimized: true,
+  },
+  webpack: (config) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      '@': './src',
+    }
+    return config
+  },
+}
+
+export default nextConfig
