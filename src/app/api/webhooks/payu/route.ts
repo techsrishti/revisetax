011d25d9de8e3fbed4677fd4c3ee71772ceb61f7
Hemanth <68335586+itsmrhem@users.noreply.github.com>
2025-06-07 16:02:07 +0530

    env for aws api g/w url

--- src/app/api/webhooks/payu/route.ts
+++ src/app/api/webhooks/payu/route.ts
@@ -206,7 +206,7 @@ export async function POST(request: NextRequest) {
            const amount = Number(payment.amount);
            const invoiceDate = getFormattedDate();
            const planName = payment.Plan.name;
-           const awsLambdaURL = "https://18khwno1j0.execute-api.ap-south-2.amazonaws.com/featbilling/revisetax-generate-pdf"
+           const awsLambdaURL = process.env.AWS_LAMBDA_URL;
            const awsLambdaPayload = {   
             "name": invoiceName,
             "phone": invoicePhone,
