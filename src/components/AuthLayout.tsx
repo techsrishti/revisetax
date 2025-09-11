8e7444525b463933cf4dd7a9af75007d7677e596
Leeladharreddyb <leeladhar2719@gmail.com>
2025-09-11 02:47:33 +0000

    update: change logo click redirect from dashboard to external URL

--- src/components/AuthLayout.tsx
+++ src/components/AuthLayout.tsx
@@ -10,7 +10,7 @@ export default function AuthLayout({ children }: AuthLayoutProps) {
   const router = useRouter();
 
   const handleLogoClick = () => {
-    router.push('/dashboard');
+    router.push('https://revisetax.com');
   };
 
   return (
